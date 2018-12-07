070ae8234589fc3ea24f16222b883d9e5b049c90
John Arnone Jr <johnsarnonejr@gmail.com>
2018-12-07 00:58:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var allBandMembers = [];
+  for (var i=0; i<musicians.length; i++) {
+    allBandMembers.push(musicians[i] + "plays" + instruments)
+  }
+  return
+}
