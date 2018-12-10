980ad3d60de66d79209a3f4175ebef55d48819cf
John Arnone Jr <johnsarnonejr@gmail.com>
2018-12-10 14:20:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+const musicians = ["John", "Paul", "George", "Ringo"];
+const instruments = ["guitar", "guitar", "bass", "drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var allBandMembers = [];
+  for (var i=0; i<musicians.length; i++) {
+    allBandMembers.push(`${musicians[i]}` + " plays " + `${instruments[i]}`);
+  }
+  return allBandMembers;
+}
